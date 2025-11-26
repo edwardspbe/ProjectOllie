9d56f395a2c3f222b1bdc74972ca682aa0fccd11
Paul Edwards <paul.e@rogers.com>
2025-11-25 21:21:06 -0500

    updated to handle date rollovers.

--- sensors/crunch_pump_times.py
+++ sensors/crunch_pump_times.py
@@ -29,7 +29,8 @@ def main():
     delay = 7   # 7 seconds based on delay pushing effluent uphill... 
 
     print("Date, Day, Pump on-time, Seconds, US Gallons, Liters")
-    with open(sys.argv[1]) as infile:
+    try:
+      with open(sys.argv[1]) as infile:
         for line in infile :
             if line.startswith('202') :
                 line = line.replace(',', '')
@@ -39,32 +40,42 @@ def main():
                     continue
                 t1 = datetime.strptime("%s %s" % (date1, time1), "%Y-%m-%d %H:%M:%S")
                 #are we calculating on-time for a new day?
-                if current_date != "" and current_date != date1 :
+                if current_date != date1 :
                     volume = current_day_total * (flowrate/60)
-                    print("%s, %s, Pump on-time, %d, %.1f, %.1f" % \
-                            (current_date, current_day, current_day_total, volume, volume*g_to_l))
+                    if current_day_total > 0 :
+                        print("%s, %s, Pump on-time, %d, %.1f, %.1f" % \
+                              (current_date, current_day, current_day_total, volume, volume*g_to_l))
                     current_day_total = 0
 
-                #TODO: figure out date roll-over...
-
-                #are we calculating on-tie for the first time or a change of date?
+                #are we calculating on-time value for the first time or a change of date?
                 #then set up a current values
                 if current_date == 0 or current_date != date1 :
                     current_day = day1
                     current_date = date1
                     current_day_total = 0
     
-                try: 
-                    line = next(infile)
-                except:
-                    dprint("Missing next line: expected OFF State\n")
+                while 1: 
+                   line = next(infile)
+                   line = line.replace(',', '')
+                   date2, day2, time2, static2, state2 = line.strip().split()
+                   #if working properly, ON should be followed by an OFF
+                   if state2 != "OFF":
+                       dprint("expected OFF state, resetting current ON values...\n")
+                       if current_date != date2 :
+                           current_day = day2
+                           current_date = date2
+                       t1 = datetime.strptime("%s %s" % (date2, time2), "%Y-%m-%d %H:%M:%S")
+                       #assume we got an ON: continue loop to read our new OFF or repeat
+                       continue
+                   else :
+                       #got our OFF so get out of forever loop
+                       break
+                    
+                if current_date != date2 :
+                    dprint("Date rollover during OFF state, going to next line to prevent errors.\n")
+                    dprint("Because we are in the OFF state, we can safely throw this away...\n")
                     continue
-                line = line.replace(',', '')
-                date2, day2, time2, static2, state2 = line.strip().split()
-                #if working properly, ON should be followed by an OFF
-                if state2 != "OFF":
-                    dprint("expected OFF state, going to next line\n")
-                    continue
+                
                 t2 = datetime.strptime("%s %s" % (date2, time2), "%Y-%m-%d %H:%M:%S")
                 elapsed = t2 - t1
                 current_day_total += elapsed.total_seconds() - delay #cause we only poll every other second.
@@ -72,14 +83,20 @@ def main():
                 dprint(current_day_total)
             else:
                 dprint(line)
+    except EOFError:
+        dprint("Found end-of-file.\n")
+
+    except Exception as e:
+        dprint(f"An unexpected error occurred: {e}")
+
     if current_day_total != 0 :
         volume = current_day_total * (flowrate/60)
-        print("%s, %s, Pump on-time, %d, %.1f, %.1f" % \
-               (current_date, current_day, current_day_total, volume, volume*g_to_l))
+        if current_day_total > 0 :
+            print("%s, %s, Pump on-time, %d, %.1f, %.1f" % \
+                  (current_date, current_day, current_day_total, volume, volume*g_to_l))
     else :
-        date = sys.argv[1].split('.')[1]
-        day = datetime.strptime(date, "%Y-%m-%d").strftime("%a")
-        print("%s, %s, Pump on-time, 0, 0, 0" % (date,day))
+        day = datetime.strptime(current_date, "%Y-%m-%d").strftime("%a")
+        print("%s, %s, Pump on-time, 0, 0, 0" % (current_date,day))
 
 if __name__ == "__main__":
   main()
